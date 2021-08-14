32a5c3a4b01d1f8a0c054f204a1bafeaf5d7c9d5
Dhaval Makawana <dhavalmakawana191@gmail.com>
2021-08-14 21:40:24 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Restaurant from "./component/Resturant";
+
+function App() {
+  return (
+    <div className="App">
+      <Restaurant />
+    </div>
+  );
+}
+
+export default App;
